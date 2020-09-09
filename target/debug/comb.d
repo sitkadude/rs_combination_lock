9c15ebf6b55403484790a31946b2415c1ab81873
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2020-09-09 11:32:12 -0400

    first commit, working program

---
+++ target/debug/comb.d
@@ -0,0 +1,1 @@
+/home/lukasjorgensen/CODE/sitkadude/rs_combination_lock/target/debug/comb: /home/lukasjorgensen/CODE/sitkadude/rs_combination_lock/src/main.rs
---
+++ target/debug/deps/comb-01e9759a940dec75.d
@@ -0,0 +1,5 @@
+/home/lukasjorgensen/CODE/sitkadude/rs_combination_lock/target/debug/deps/comb-01e9759a940dec75: src/main.rs
+
+/home/lukasjorgensen/CODE/sitkadude/rs_combination_lock/target/debug/deps/comb-01e9759a940dec75.d: src/main.rs
+
+src/main.rs:
